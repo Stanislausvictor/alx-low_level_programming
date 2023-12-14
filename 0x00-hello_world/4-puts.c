d6f87653d5bfe5a77fb296fda703e56f86289db1
Stanislausvictor <emeremginivictor@gmail.com>
2023-12-14 12:34:28 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that prints a ling with a put function
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts('\"Programming is like building a multilingual puzzle,");
+return (0);
+}
